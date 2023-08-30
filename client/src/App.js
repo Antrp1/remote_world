491249e94cbec24793077503c46020665a6f2a24
saanspeur <evelin.ortega018@gmail.com>
2023-08-30 02:30:01 -0700

    added bio

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,11 @@ import "semantic-ui-css/semantic.min.css";
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 // import { motion } from "framer-motion";
-import ContactUs from './Segments/ContactUs';
-import Home from './Pages/Home';
-import Signup from './Pages/Signup';
-import Jobs from './Pages/Jobs';
-import NavBar from './Segments/NavBar'
+import ContactUs from "./Segments/ContactUs";
+import Home from "./Pages/Home";
+import Signup from "./Pages/signup";
+import Jobs from "./Pages/Jobs";
+import NavBar from "./Segments/NavBar";
 
 /* Heads up!
  * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
